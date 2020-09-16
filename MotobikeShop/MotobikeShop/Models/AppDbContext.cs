6f3761be01cff2e78e61078fe85840ece19d12a7
lethienhongson96 <66103162+lethienhongson96@users.noreply.github.com>
2020-09-16 19:56:55 +0700

    HongSon-Migration And SeeData

---
+++ MotobikeShop/MotobikeShop/Models/AppDbContext.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MotobikeShop.Configurations;
+using MotobikeShop.Extensions;
+using MotobikeShop.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MotobikeShop.Models
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext()
+        {
+
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        //Address for Account
+        public DbSet<District> District { get; set; }
+        public DbSet<Province> Province { get; set; }
+        public DbSet<Ward> Ward { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderDetailConfiguration());
+            modelBuilder.ApplyConfiguration(new ApplicationUserConfiguration());
+
+            //config Address 
+            modelBuilder.ApplyConfiguration(new ProvinceConfiguration());
+            modelBuilder.ApplyConfiguration(new DistrictConfiguration());
+            modelBuilder.ApplyConfiguration(new WardConfiguration());
+
+            //seeding data
+            modelBuilder.Seed();
+        }
+    }
+}
