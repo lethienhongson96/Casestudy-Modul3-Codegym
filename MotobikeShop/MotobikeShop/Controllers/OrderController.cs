1e6b69eb77241d744a4fc8d6616038a72e1be0e9
lethienhongson96 <66103162+lethienhongson96@users.noreply.github.com>
2020-09-17 19:52:44 +0700

    HongSon-Create Customer view and controller, star page with Customer view Index

--- MotobikeShop/MotobikeShop/Controllers/OrderController.cs
+++ MotobikeShop/MotobikeShop/Controllers/OrderController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MotobikeShop.Enums;
 using MotobikeShop.Models.Entities;
 using MotobikeShop.Repositories;
 
--- MotobikeShop/MotobikeShop/Startup.cs
+++ MotobikeShop/MotobikeShop/Startup.cs
@@ -73,7 +73,7 @@ namespace MotobikeShop
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Customer}/{action=Index}/{id?}");
             });
         }
     }
