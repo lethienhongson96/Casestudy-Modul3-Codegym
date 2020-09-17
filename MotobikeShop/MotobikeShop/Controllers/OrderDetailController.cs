07442225e5ee0a765f88e746b86eeafac4b35888
lethienhongson96 <66103162+lethienhongson96@users.noreply.github.com>
2020-09-17 07:02:44 +0700

    HongSon-Create orderdetailController Write method Create orderdetail when user add product to a order and watch items in a order, calculator money for each orderdetail method

---
+++ MotobikeShop/MotobikeShop/Controllers/OrderDetailController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MotobikeShop.Models;
+using MotobikeShop.Models.Entities;
+using MotobikeShop.Repositories;
+
+namespace MotobikeShop.Controllers
+{
+    public class OrderDetailController : Controller
+    {
+        private readonly IOrderDetailRepository detailRepository;
+
+        public OrderDetailController(IOrderDetailRepository detailRepository)
+        {
+            this.detailRepository = detailRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WatchOrderDetail(int id) =>
+            View(detailRepository.GetOrderByid(id));
+
+        [HttpGet]
+        public IActionResult Create(int id) =>
+            View(detailRepository.CreateByOrderId(id));
+
+        [HttpPost]
+        public IActionResult Create(OrderDetail orderDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                if (detailRepository.CreateOrderDetail(orderDetail) > 0 && orderDetail.Quantity > 0)
+                    return RedirectToAction("WatchOrderDetail", "OrderDetail", detailRepository.GetOrderByid(orderDetail.OrderId));
+                else
+                    ModelState.AddModelError("", "Quantity is not Null");
+            }
+            else
+                ModelState.AddModelError("", "Some thing wrong");
+
+            return View(orderDetail);
+        }
+
+        public JsonResult GetProductsByCategoryId(int id) =>
+             Json(new SelectList(detailRepository.GetListProductByCategoryId(id), "Id", "Name"));
+
+        [Route("/OrderDetail/GetPrice/{ProductId}/{Discount}/{Quantity}")]
+        public JsonResult GetPrice(int ProductId, int Discount, int Quantity)
+        {
+            Product product = detailRepository.GetProductById(ProductId);
+
+            return Json(detailRepository.CalculateMoney(product.PricePerUnit, Discount, Quantity));
+        }
+
+        public JsonResult DefaultByProductId(int id)
+        {
+            Product product = detailRepository.GetProductById(id);
+
+            return Json(product.PricePerUnit);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int ProductId, int OrderId)
+        {
+            return View(detailRepository.GetOrderDetailByIds(ProductId, OrderId));
+        }
+
+        [HttpPost]
+        public IActionResult Edit(OrderDetail orderDetail)
+        {
+            if (detailRepository.UpdateOrderDetail(orderDetail) > 0)
+            {
+                return RedirectToAction("WatchOrderDetail", "OrderDetail", detailRepository.GetOrderByid(orderDetail.OrderId));
+            }
+
+            return View(orderDetail);
+        }
+
+        [Route("/OrderDetail/Delete/{OrderId}/{ProductId}")]
+        public IActionResult Delete(int OrderId, int ProductId)
+        {
+            var result = detailRepository.DeleteOrderDetail(OrderId, ProductId);
+            return Json(new { result });
+        }
+    }
+}
