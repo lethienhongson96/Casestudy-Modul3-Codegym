6dc885b0c376bd22e18967a5b100e6ef063086c9
lethienhongson96 <66103162+lethienhongson96@users.noreply.github.com>
2020-09-22 08:46:57 +0700

    HongSon-Get Layout for sale

--- MotobikeShop/MotobikeShop/Startup.cs
+++ MotobikeShop/MotobikeShop/Startup.cs
@@ -81,7 +81,7 @@ namespace MotobikeShop
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=CustomerHome}/{action=Index}/{id?}");
             });
         }
     }
